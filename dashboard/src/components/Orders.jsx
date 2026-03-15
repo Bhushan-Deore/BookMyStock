8e93090be04cb2ad6123076a05863920888d31ad
Bhushan Deore <bdeore645@gmail.com>
2026-03-15 21:21:19 +0530

    first commit

---
+++ dashboard/src/components/Orders.jsx
@@ -0,0 +1,62 @@
+import { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Orders = () => {
+  const [allOrders,setAllOrders] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:4000/allOrders").then((res) => {
+        console.log(res.data);
+        setAllOrders(res.data);
+      });
+  },[])
+
+
+
+  return (
+    <>
+      <h3 className="title">Open orders({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <thead>
+          <tr>
+            <th>Type</th>
+            <th>Instrument</th>
+            <th>Product</th>
+            <th>Qty.</th>
+            <th>Price</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          {allOrders.map((stock, index) => {
+            return (
+              <tr key={index}>
+                <td>{stock.mode}</td>
+                <td>{stock.name}</td>
+                <td>CNC</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price.toFixed(2)}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+      </div>
+    
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Orders;
