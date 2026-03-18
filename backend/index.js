dd24aa091a208a0bde9897059a1f1853b402d568
Bhushan Deore <bdeore645@gmail.com>
2026-03-18 15:29:44 +0530

    redirect issue 3

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,15 @@ const allowedOrigins = [
     "http://localhost:5174",
 ];
 
-
+    
 app.use(cors({
     origin: allowedOrigins,
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", authRoute);
